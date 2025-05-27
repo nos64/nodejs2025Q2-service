07d966ef968dd58e9cc5e72e7efcd4f48be10f8d
Mikhail Nosov <manosov1984@gmail.com>
2025-05-27 21:32:53 +0300

    feat: generate all controllers with nest CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './users/user.module';
+import { ArtistModule } from './artists/artist.module';
+import { TrackModule } from './tracks/track.module';
+import { AlbumModule } from './albums/album.module';
+import { FavsModule } from './favs/favs.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavsModule],
   controllers: [AppController],
   providers: [AppService],
 })
