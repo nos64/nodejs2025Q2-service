1243cf53bf35d02dc6076c29ca6d1bda08a0fdb6
Mikhail Nosov <manosov1984@gmail.com>
2025-05-30 00:51:34 +0300

    feat: update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  dotenv.config();
+  const PORT = Number(process.env.PORT) || 4000;
+  await app.listen(PORT);
 }
 bootstrap();
