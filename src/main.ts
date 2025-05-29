feb6183ce42d069c7a8797c4b95276ff459fe6e7
Mikhail Nosov <manosov1984@gmail.com>
2025-05-30 02:17:43 +0300

    feat: add generate open API dock on «http://localhost:4000/api»

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 import { AppModule } from './app.module';
@@ -6,8 +7,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription('Home Library Service')
+    .setVersion('1.0')
+    .addTag('Home music library service')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   dotenv.config();
-  const PORT = Number(process.env.PORT) || 4000;
+  const PORT = Number(process.env.PORT) ?? 4000;
   await app.listen(PORT);
 }
 bootstrap();
