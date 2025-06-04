cfcffa8f590a4f542c7d7742283a432449accfb4
Mikhail Nosov <manosov1984@gmail.com>
2025-06-04 08:35:07 +0300

    feat: replace in-memory data with postgres prisma in Favorites layer

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
-import { DataBaseModule } from 'src/data-base/data-base.module';
 
 @Module({
-  imports: [DataBaseModule],
   controllers: [FavoritesController],
   providers: [FavoritesService],
   exports: [FavoritesService],
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -4,32 +4,97 @@ import {
   UnprocessableEntityException,
 } from '@nestjs/common';
 
-import { DataBaseService } from 'src/data-base/data-base.service';
 import { FavoritesResponse } from './entities/favorites-response.entity';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { type FavoriteEntryName, Favorites } from './entities/favorite.entity';
-
 @Injectable()
 export class FavoritesService {
-  constructor(private databaseService: DataBaseService) {}
+  constructor(private prismaService: PrismaService) {}
+  private async isEntityInFavorites(
+    id: string,
+    entityFavType: keyof Favorites,
+  ): Promise<boolean> {
+    switch (entityFavType) {
+      case 'artists':
+        return !!(await this.prismaService.favoriteArtist.findUnique({
+          where: { artistId: id },
+        }));
+
+      case 'albums':
+        return !!(await this.prismaService.favoriteAlbum.findUnique({
+          where: { albumId: id },
+        }));
+      case 'tracks':
+        return !!(await this.prismaService.favoriteTrack.findUnique({
+          where: { trackId: id },
+        }));
+
+      default:
+        throw new UnprocessableEntityException(
+          `Invalid entity type: ${entityFavType}`,
+        );
+    }
+  }
+
+  async isEntityExists(
+    id: string,
+    entityType: keyof Favorites,
+  ): Promise<boolean> {
+    const model = entityType.slice(0, -1);
+
+    return !!(await this.prismaService[model].findUnique({
+      where: { id },
+    }));
+  }
+
   private async addEntityToFav(
     id: string,
     entityFavType: keyof Favorites,
     entityString: FavoriteEntryName,
   ) {
-    if (await this.databaseService.isEntityInFavorites(id, entityFavType)) {
-      return `This ${entityString} has already been added to favorites before`;
+    if (await this.isEntityInFavorites(id, entityFavType)) {
+      throw new UnprocessableEntityException(
+        `This ${entityString} has already been added to favorites before`,
+      );
     }
 
-    const isExist = await this.databaseService.isEntityExists(
-      id,
-      entityFavType,
-    );
-
-    if (!isExist) {
+    if (!(await this.isEntityExists(id, entityFavType))) {
       throw new UnprocessableEntityException(`${entityString} does not exist`);
     }
 
-    await this.databaseService.addToFavorites(id, entityFavType);
+    switch (entityFavType) {
+      case 'artists':
+        await this.prismaService.favoriteArtist.create({
+          data: {
+            artistId: id,
+          },
+        });
+
+        break;
+
+      case 'albums':
+        await this.prismaService.favoriteAlbum.create({
+          data: {
+            albumId: id,
+          },
+        });
+
+        break;
+
+      case 'tracks':
+        await this.prismaService.favoriteTrack.create({
+          data: {
+            trackId: id,
+          },
+        });
+
+        break;
+
+      default:
+        throw new UnprocessableEntityException(
+          `Invalid entity type: ${entityFavType}`,
+        );
+    }
 
     return `${entityString} successfully added to favorites`;
   }
@@ -39,16 +104,45 @@ export class FavoritesService {
     entityFavType: keyof Favorites,
     entityString: FavoriteEntryName,
   ) {
-    const isEntityInFav = await this.databaseService.isEntityInFavorites(
-      id,
-      entityFavType,
-    );
+    const isEntityInFav = await this.isEntityInFavorites(id, entityFavType);
 
     if (!isEntityInFav) {
       throw new NotFoundException(`This ${entityString} is not favorite`);
     }
 
-    await this.databaseService.removeFromFavorites(id, entityFavType);
+    switch (entityFavType) {
+      case 'artists':
+        await this.prismaService.favoriteArtist.delete({
+          where: {
+            artistId: id,
+          },
+        });
+
+        break;
+
+      case 'albums':
+        await this.prismaService.favoriteAlbum.delete({
+          where: {
+            albumId: id,
+          },
+        });
+
+        break;
+
+      case 'tracks':
+        await this.prismaService.favoriteTrack.delete({
+          where: {
+            trackId: id,
+          },
+        });
+
+        break;
+
+      default:
+        throw new UnprocessableEntityException(
+          `Invalid entity type: ${entityFavType}`,
+        );
+    }
 
     return `${entityString} successfully removed from favorites`;
   }
@@ -66,7 +160,23 @@ export class FavoritesService {
   }
 
   async findAll(): Promise<FavoritesResponse> {
-    return await this.databaseService.getFullFavorites();
+    const [artists, albums, tracks] = await Promise.all([
+      this.prismaService.favoriteArtist.findMany({
+        include: { artist: true },
+      }),
+      this.prismaService.favoriteAlbum.findMany({
+        include: { album: true },
+      }),
+      this.prismaService.favoriteTrack.findMany({
+        include: { track: true },
+      }),
+    ]);
+
+    return {
+      artists: artists.map((fav) => fav.artist),
+      albums: albums.map((fav) => fav.album),
+      tracks: tracks.map((fav) => fav.track),
+    };
   }
 
   async removeArtist(id: string): Promise<string> {
@@ -82,14 +192,14 @@ export class FavoritesService {
   }
 
   async isAlbumInFavorites(id: string) {
-    return await this.databaseService.isEntityInFavorites(id, 'albums');
+    return await this.isEntityInFavorites(id, 'albums');
   }
 
   async isArtistInFavorites(id: string) {
-    return await this.databaseService.isEntityInFavorites(id, 'artists');
+    return await this.isEntityInFavorites(id, 'artists');
   }
 
   async isTrackInFavorites(id: string) {
-    return await this.databaseService.isEntityInFavorites(id, 'tracks');
+    return await this.isEntityInFavorites(id, 'tracks');
   }
 }
