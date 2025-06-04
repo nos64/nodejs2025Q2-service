65d99b0ca4594c2077f32a8c40b15aeda0467761
Mikhail Nosov <manosov1984@gmail.com>
2025-06-04 08:34:45 +0300

    feat: replace in-memory data with postgres prisma in Track layer

--- src/tracks/track.module.ts
+++ src/tracks/track.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TrackService } from './track.service';
 import { TrackController } from './track.controller';
-import { DataBaseModule } from 'src/data-base/data-base.module';
 import { FavoritesModule } from 'src/favorites/favorites.module';
 
 @Module({
-  imports: [DataBaseModule, FavoritesModule],
+  imports: [FavoritesModule],
   controllers: [TrackController],
   providers: [TrackService],
   exports: [TrackService],
--- src/tracks/track.service.ts
+++ src/tracks/track.service.ts
@@ -1,40 +1,42 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { v4 as uuid } from 'uuid';
 
-import { DataBaseService } from 'src/data-base/data-base.service';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
-import { Track } from './entities/track.entity';
 import { FavoritesService } from 'src/favorites/favorites.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Track } from '@prisma/client';
 
 @Injectable()
 export class TrackService {
   constructor(
-    private databaseService: DataBaseService,
+    private prismaService: PrismaService,
     private favoritesService: FavoritesService,
   ) {}
 
-  async create(createTrackDto: CreateTrackDto) {
+  async create(createTrackDto: CreateTrackDto): Promise<Track> {
     const { name, duration } = createTrackDto;
-    const createdTrack = {
-      id: uuid(),
-      name,
-      artistId: createTrackDto.artistId ?? null,
-      albumId: createTrackDto.albumId ?? null,
-      duration,
-    };
-
-    await this.databaseService.createTrack(createdTrack);
+    const createdTrack = await this.prismaService.track.create({
+      data: {
+        name,
+        artistId: createTrackDto.artistId ?? null,
+        albumId: createTrackDto.albumId ?? null,
+        duration,
+      },
+    });
 
     return createdTrack;
   }
 
-  async findAll() {
-    return await this.databaseService.getTracks();
+  async findAll(): Promise<Track[]> {
+    return await this.prismaService.track.findMany();
   }
 
-  async findOne(id: string) {
-    const track = await this.databaseService.getTrackById(id);
+  async findOne(id: string): Promise<Track> {
+    const track: Track = await this.prismaService.track.findUnique({
+      where: {
+        id,
+      },
+    });
 
     if (!track) {
       throw new NotFoundException('Track not found');
@@ -43,38 +45,54 @@ export class TrackService {
     return track;
   }
 
-  async update(id: string, updateTrackDto: UpdateTrackDto) {
-    const track = await this.databaseService.getTrackById(id);
+  async update(id: string, updateTrackDto: UpdateTrackDto): Promise<Track> {
+    const track = await this.prismaService.track.findUnique({
+      where: {
+        id,
+      },
+    });
 
     if (!track) {
       throw new NotFoundException('Track not found');
     }
 
-    const updatedTrack: Track = {
-      ...track,
-      name: updateTrackDto.name || track.name,
-      artistId: updateTrackDto?.artistId || null,
-      albumId: updateTrackDto?.albumId ?? null,
-      duration: updateTrackDto?.duration,
-    };
-
-    await this.databaseService.updateTrack(id, updatedTrack);
+    const updatedTrack: Track = await this.prismaService.track.update({
+      where: {
+        id,
+      },
+      data: {
+        ...track,
+        name: updateTrackDto.name || track.name,
+        artistId: updateTrackDto?.artistId || null,
+        albumId: updateTrackDto?.albumId ?? null,
+        duration: updateTrackDto?.duration,
+      },
+    });
 
     return updatedTrack;
   }
 
-  async remove(id: string) {
-    const track = await this.databaseService.getTrackById(id);
-
-    if (!track) {
-      throw new NotFoundException('Track not found');
-    }
-    await this.databaseService.deleteTrack(id);
-
+  async remove(id: string): Promise<void> {
     const isArtistInFavs = await this.favoritesService.isTrackInFavorites(id);
 
     if (isArtistInFavs) {
       await this.favoritesService.removeTrack(id);
     }
+
+    const track: Track = await this.prismaService.track.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!track) {
+      throw new NotFoundException('Track not found');
+    }
+
+    await this.prismaService.track.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
