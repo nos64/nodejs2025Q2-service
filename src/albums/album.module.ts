ff6ad44b2a2fae11bf1474da537952a83b560bed
Mikhail Nosov <manosov1984@gmail.com>
2025-06-04 08:34:21 +0300

    feat: replace in-memory data with postgres prisma in Album layer

--- src/albums/album.module.ts
+++ src/albums/album.module.ts
@@ -2,17 +2,11 @@ import { forwardRef, Module } from '@nestjs/common';
 import { AlbumService } from './album.service';
 import { AlbumController } from './album.controller';
 import { FavoritesModule } from 'src/favorites/favorites.module';
-import { DataBaseModule } from 'src/data-base/data-base.module';
 import { ArtistModule } from 'src/artists/artist.module';
 import { TrackModule } from 'src/tracks/track.module';
 
 @Module({
-  imports: [
-    DataBaseModule,
-    FavoritesModule,
-    TrackModule,
-    forwardRef(() => ArtistModule),
-  ],
+  imports: [FavoritesModule, TrackModule, forwardRef(() => ArtistModule)],
   controllers: [AlbumController],
   providers: [AlbumService],
   exports: [AlbumService],
--- src/albums/album.service.ts
+++ src/albums/album.service.ts
@@ -4,46 +4,48 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { v4 as uuid } from 'uuid';
 
+import { Album } from '@prisma/client';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
-import { DataBaseService } from 'src/data-base/data-base.service';
-import { Album } from './entities/album.entity';
 import { FavoritesService } from 'src/favorites/favorites.service';
 import { ArtistService } from 'src/artists/artist.service';
 import { TrackService } from 'src/tracks/track.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AlbumService {
   constructor(
-    private databaseService: DataBaseService,
+    private prismaService: PrismaService,
     private favoritesService: FavoritesService,
     private trackService: TrackService,
     @Inject(forwardRef(() => ArtistService))
     private artistService: ArtistService,
   ) {}
 
-  async create(createAlbumDto: CreateAlbumDto) {
+  async create(createAlbumDto: CreateAlbumDto): Promise<Album> {
     const { name, year } = createAlbumDto;
-    const createdAlbum: Album = {
-      id: uuid(),
-      name,
-      year,
-      artistId: createAlbumDto.artistId ?? null,
-    };
-
-    await this.databaseService.createAlbum(createdAlbum);
+    const createdAlbum = await this.prismaService.album.create({
+      data: {
+        name,
+        year,
+        artistId: createAlbumDto.artistId ?? null,
+      },
+    });
 
     return createdAlbum;
   }
 
-  async findAll() {
-    return await this.databaseService.getAlbums();
+  async findAll(): Promise<Album[]> {
+    return await this.prismaService.album.findMany();
   }
 
-  async findOne(id: string) {
-    const album = await this.databaseService.getAlbumById(id);
+  async findOne(id: string): Promise<Album> {
+    const album: Album = await this.prismaService.album.findUnique({
+      where: {
+        id,
+      },
+    });
 
     if (!album) {
       throw new NotFoundException('Album not found');
@@ -52,8 +54,12 @@ export class AlbumService {
     return album;
   }
 
-  async update(id: string, updateAlbumDto: UpdateAlbumDto) {
-    const album = await this.databaseService.getAlbumById(id);
+  async update(id: string, updateAlbumDto: UpdateAlbumDto): Promise<Album> {
+    const album = await this.prismaService.album.findUnique({
+      where: {
+        id,
+      },
+    });
 
     if (!album) {
       throw new NotFoundException('Album not found');
@@ -68,27 +74,22 @@ export class AlbumService {
       }
     }
 
-    const updatedAlbum: Album = {
-      ...album,
-      name: updateAlbumDto?.name || album.name,
-      year: updateAlbumDto?.year || album.year,
-      artistId: updateAlbumDto?.artistId ?? null,
-    };
-
-    await this.databaseService.updateAlbum(id, updatedAlbum);
+    const updatedAlbum: Album = await this.prismaService.album.update({
+      where: {
+        id,
+      },
+      data: {
+        ...album,
+        name: updateAlbumDto?.name || album.name,
+        year: updateAlbumDto?.year || album.year,
+        artistId: updateAlbumDto?.artistId ?? null,
+      },
+    });
 
     return updatedAlbum;
   }
 
-  async remove(id: string) {
-    const album = await this.databaseService.getAlbumById(id);
-
-    if (!album) {
-      throw new NotFoundException('Album not found');
-    }
-
-    await this.databaseService.deleteAlbum(id);
-
+  async remove(id: string): Promise<void> {
     const tracks = await this.trackService.findAll();
     tracks.map(async (track) =>
       track.albumId === id
@@ -104,5 +105,21 @@ export class AlbumService {
     if (isAlbumInFavs) {
       await this.favoritesService.removeAlbum(id);
     }
+
+    const album: Album = await this.prismaService.album.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!album) {
+      throw new NotFoundException('Album not found');
+    }
+
+    await this.prismaService.album.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
