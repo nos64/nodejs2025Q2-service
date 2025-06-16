e428cb1b36eb0321090e79330eec85b7e5d0a983
Mikhail Nosov <manosov1984@gmail.com>
2025-06-16 22:14:40 +0300

    feat: add JwtAccessTokenStrategy & JwtRefreshTokenStrategy

---
+++ src/auth/jwt-access-token.strategy.ts
@@ -0,0 +1,21 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { TokenPayload } from './dto/token-payload.dto';
+
+@Injectable()
+export class JwtAccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET_KEY'),
+    });
+  }
+
+  async validate(payload: TokenPayload) {
+    return payload;
+  }
+}
