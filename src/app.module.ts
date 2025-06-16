126bae54ac41a3d6ccf26b3c05188ce4640a5474
Mikhail Nosov <manosov1984@gmail.com>
2025-06-16 22:10:24 +0300

    feat: add AuthModule & global JwtAccessTokenGuard to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/user.module';
@@ -10,6 +13,8 @@ import { PrismaModule } from './prisma/prisma.module';
 import { LoggingModule } from './logging/logging.module';
 import { LoggingService } from './logging/logging.service';
 import { LoggerMiddleware } from './logging/logging.middleware';
+import { AuthModule } from './auth/auth.module';
+import { JwtAccessTokenGuard } from './common/guards/jwt-access-token.guard';
 
 @Module({
   imports: [
@@ -20,9 +25,18 @@ import { LoggerMiddleware } from './logging/logging.middleware';
     FavoritesModule,
     PrismaModule,
     LoggingModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService, LoggingService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAccessTokenGuard,
+    },
+    AppService,
+    LoggingService,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
