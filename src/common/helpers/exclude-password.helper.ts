2156759ff9501afb0c56e1b4b865efab230d329b
Mikhail Nosov <manosov1984@gmail.com>
2025-06-16 22:11:49 +0300

    feat: update excludePassword (add refreshToken)

--- src/common/helpers/exclude-password.helper.ts
+++ src/common/helpers/exclude-password.helper.ts
@@ -3,7 +3,7 @@ import { UserResponse } from 'src/users/entities/user-response.entry';
 
 export function excludePassword(user: User): UserResponse {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { password, createdAt, updatedAt, ...rest } = user;
+  const { password, refreshToken, createdAt, updatedAt, ...rest } = user;
   return {
     ...rest,
     createdAt: user.createdAt.getTime(),
